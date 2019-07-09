d7a42f81664b0364d6a25bf309b6e91497184701
moubella <sudofalcon@gmail.com>
2019-07-09 17:32:23 +0200

    added a CSS style varible example

--- reactapp/src/components/TodoItem.js
+++ reactapp/src/components/TodoItem.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
     render() {
         return (
-            <div>
+            <div style={itemStyle}>
                 <p>{this.props.todo.title}</p>
             </div>
         )
@@ -18,5 +18,8 @@ TodoItem.propTypes = {
     todos : PropTypes.object.isRequired
 }
 
+const itemStyle = {
+    backgroundColor : '#f4f4f4'
+}
 
 export default TodoItem
